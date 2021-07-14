91106c2000b6ba5faff687fb3f934db1094b1b11
antalmelinda1116 <antalmelinda1116@gmail.com>
2021-07-14 18:16:46 +0000

    result styled

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 $( document ).ready(function() {
   
-    $('#search-btn').click(function(){
-        var text=jQuery('#search-input').val();
-        var url = 'https://gateway.marvel.com/v1/public/characters?apikey=5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34&ts=1&name='+text;
-        $.ajax({
-          url: url,
-          success: function(result){
-            if (result.status=='Ok'){
-                var character=result.data.results;
-                $(character).each(function(){
-                    var html='<span>'+this.name+'</span>';
-                    html+='<img>'+this.thumbnail.path;
-    
-                    $('#data').append(html);
-                })
-            }
-            console.log(result.code);
+  $('#search-btn').click(function(){
+      var text=jQuery('#search-input').val();
+      var url = 'https://gateway.marvel.com/v1/public/characters?apikey=5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34&ts=1&name='+text;
+      $.ajax({
+        url: url,
+        success: function(result){
+          if (result.status=='Ok'){
+              var character=result.data.results;
+              $(character).each(function(){
+                  var html='<span>'+this.name+'</span>';
+                  html+='<img>'+this.thumbnail.path;
+  
+                  $('#data').append(html);
+              })
           }
-        });
-        });
-    
-    });
+          console.log(result.code);
+        }
+      });
+      });
+  
+  });
