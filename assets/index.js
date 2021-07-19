de5a7244ca2e9770480bf9c1bc875df904b387d1
antalmelinda1116 <antalmelinda1116@gmail.com>
2021-07-19 15:01:45 +0000

    Show all function created

--- assets/index.js
+++ assets/index.js
@@ -8,6 +8,14 @@ $( document ).ready(function() {
   }
      
    });
+
+   $('#show-all-btn').click(function(){
+    $('#data').html('');
+    
+     getAllHeroes();
+ 
+    
+  });
  
    const getSingleHeroe = (text)=>{
    fetch('https://gateway.marvel.com/v1/public/characters?apikey=5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34&ts=1&limit=100&name='+text)
@@ -17,6 +25,15 @@ $( document ).ready(function() {
    .catch(error=>console.log(error))
  
  }
+
+ const getAllHeroes = ()=>{
+  fetch('https://gateway.marvel.com/v1/public/characters?apikey=5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34&ts=1&limit=100')
+  .then(res=>res.json())
+  .then(data=>extractCorrectData(data.data.results))
+  .then(result=>showResults(result))
+  .catch(error=>console.log(error))
+
+}
  
    const extractCorrectData = (character) => {
    return character.map(item =>{
@@ -43,7 +60,7 @@ $( document ).ready(function() {
    
    results.forEach(element => {
      let html = `
-      <div class='col col-md-6 col-lg-3'>
+      <div class='col col-md-4 col-lg-3 col-xl-2'>
         <div class='box'>
           <a href='javascript:getCharacter(${element.id})'>
             <img src='${element.imageURL}' alt=''>
@@ -51,7 +68,6 @@ $( document ).ready(function() {
               <h2>${element.name}</h2>
             </div>
           </a>
-          <p>${element.description}</p>
         </div>
       </div>`
      $('#data').append(html);
