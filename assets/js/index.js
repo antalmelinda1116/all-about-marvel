0f65661dc2e5e27b398cb791c689cf6d6f2f007e
antalmelinda1116 <antalmelinda1116@gmail.com>
2021-07-25 00:09:47 +0000

    Semicolons corrected

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,18 +25,18 @@ $( document ).ready(function() {
    .then(res=>res.json())
    .then(data=>extractCorrectData(data.data.results))
    .then(result=>showResults(result))
-   .catch(error=>console.log(error))
+   .catch(error=>console.log(error));
  
- }
+ };
 
  const getAllHeroes = ()=>{
   fetch('https://gateway.marvel.com/v1/public/characters?apikey=5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34&ts=1&limit=100')
   .then(res=>res.json())
   .then(data=>extractCorrectData(data.data.results))
   .then(result=>showResults(result))
-  .catch(error=>console.log(error))
+  .catch(error=>console.log(error));
 
-}
+};
  
    const extractCorrectData = (character) => {
    return character.map(item =>{
@@ -46,10 +46,10 @@ $( document ).ready(function() {
        name: item.name,
        description: item.description,
        imageURL: `${item.thumbnail.path}`+'/portrait_incredible'+`.${item.thumbnail.extension}`.replace(/^http:W/i,'https://')
-     }
+     };
    
-   })
- } 
+   });
+ }; 
    
  }); 
  
@@ -57,7 +57,7 @@ $( document ).ready(function() {
 
   if (results.length==0){
     let html = `
-    <h2>Sorry, we couldn't find any superheroes with the given character name. Please check if the spelling is right.</h2>`
+    <h2>Sorry, we couldn't find any superheroes with the given character name. Please check if the spelling is right.</h2>`;
     $('#data').html(html);
   }
    
@@ -72,18 +72,18 @@ $( document ).ready(function() {
             </div>
           </a>
         </div>
-      </div>`
+      </div>`;
      $('#data').append(html);
    });
    $('#search-btn').attr('disabled',false);
    $('#spinner-modal').modal('hide');
    
- }
+ };
 
  function getCharacter(id) {
   fetch('https://gateway.marvel.com/v1/public/characters/'+id+'?apikey=5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34&ts=1')
   .then(res=>res.json())
-  .then(result=>singleResult(result))
+  .then(result=>singleResult(result));
 }
 
 const singleResult = (results) => {
@@ -98,6 +98,6 @@ const singleResult = (results) => {
       <h4>Description:</h4>
       <p>${character.description}</p>
     </div>
-  </div>`
+  </div>`;
   $('#data').html(html);
-}
+};
