98c448a94ff1054f871b8939cff6b7afb36fdb4c
antalmelinda1116 <antalmelinda1116@gmail.com>
2021-09-23 13:10:23 +0000

    More descriptive names and error message given

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const APIKEY = "5895d6f1aa65ad4fbf8f2e49c3db79dd&hash=5b0f850b5bd90b5b2f5b2028c4b3be34"
+const APIURL = "https://gateway.marvel.com/v1/public/characters"
 
 $( document ).ready(function() {
 
@@ -23,17 +24,17 @@ $( document ).ready(function() {
   });
  
    const getHeroes = (SearchingValue)=>{
-     let apiURL = `https://gateway.marvel.com/v1/public/characters?apikey=${APIKEY}&ts=1&limit=100`
+     let apiURL = `${APIURL}?apikey=${APIKEY}&ts=1&limit=100`
      if(SearchingValue) apiURL + `&name=${SearchingValue}`
    fetch(apiURL)
    .then(res=>res.json())
-   .then(data=>extractCorrectData(data.data.results))
+   .then(data=>getSearchResult(data.data.results))
    .then(result=>showResults(result))
    .catch(error=>console.log(error));
  
  };
  
-   const extractCorrectData = (character) => {
+   const getSearchResult = (character) => {
    return character.map(item =>{
      
      return {
@@ -49,10 +50,16 @@ $( document ).ready(function() {
  }); 
  
  const showResults = (results) => {
-
-  if (results.length==0){
+  if (results.length<=0){
     let html = `
-    <h2>Sorry, we couldn't find any superheroes with the given character name. Please check if the spelling is right.</h2>`;
+    <h2>Sorry, we couldn't find any superheroes with the given character name.</h2>
+    <p>Please check if any of the issues below is occurring:</p>
+    <ul>
+      <li>Incorrect spelling.</li>
+      <li>No given searching parameter.</li>
+      <li>Given character is not a Marvel superhero.</li>
+    </ul>
+    `;
     $('#data').html(html);
   }
    
@@ -76,7 +83,7 @@ $( document ).ready(function() {
  };
 
  function getCharacter(id) {
-  fetch(`https://gateway.marvel.com/v1/public/characters/'+id+'?apikey=${APIKEY}&ts=1`)
+  fetch(`${APIURL}/${id}'?apikey=${APIKEY}&ts=1`)
   .then(res=>res.json())
   .then(result=>singleResult(result));
 }
